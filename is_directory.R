e8c8665c86991269a93b8f99501a8a1ffe166582
Alison <33938391+Aliskid@users.noreply.github.com>
2019-04-12 10:26:02 +0200

    Update is_directory.R

--- is_directory.R
+++ is_directory.R
@@ -1,5 +1,5 @@
 if (dir.exists("./your_directory")){
-	if (length(dir("./your_directory", all.files=TRUE, pattern = "*.csv")) != 0){
+	if (length(dir("./your_directory", all.files=TRUE, pattern = "*")) != 0){
     """ do something"""
     } else {
 	print ("No file found")}
