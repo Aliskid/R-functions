5c3fb24496b40fd659887ac2d30bd1701bb89a81
Alison <33938391+Aliskid@users.noreply.github.com>
2019-04-12 10:32:24 +0200

    Update put_in_row.R

--- put_in_row.R
+++ put_in_row.R
@@ -2,3 +2,6 @@ put_in_rows <- function(path, pattern, recursive = FALSE, read_fun){
 			data.files <- list.files(path, pattern = pattern, recursive = recursive)
 			data <- lapply(paste0(path, data.files), read_fun)
 			data <- do.call("cbind", data)}
+
+#Exemple for csv files of a directory
+data <- put_in_rows("./your_path_to_directory", "*.csv", TRUE, read.csv)
